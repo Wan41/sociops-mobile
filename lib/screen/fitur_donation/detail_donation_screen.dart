7d671905575355ca45a09e346857a19fe3fddcef
dhaifan999 <dhaifan25@gmail.com>
2023-06-15 17:23:50 +0700

    update detail donation screen

--- lib/screen/fitur_donation/detail_donation_screen.dart
+++ lib/screen/fitur_donation/detail_donation_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DetailDonationScreen extends StatefulWidget {
   const DetailDonationScreen({super.key});
@@ -10,6 +13,95 @@ class DetailDonationScreen extends StatefulWidget {
 
 class _DetailDonationScreenState extends State<DetailDonationScreen> {
   bool isBookmarked = false;
+
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? _imageFileList;
+  dynamic _pickImageError;
+
+  Future<void> _onImageButtonPressed(ImageSource source) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _setImageFileListFromFile(pickedFile);
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _pickImageError = e;
+      });
+    }
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).pop();
+  }
+
+  void _setImageFileListFromFile(XFile? file) {
+    if (file != null) {
+      setState(() {
+        _imageFileList = [file];
+      });
+    }
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: 300.0,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: [
+          const Text(
+            'Pilih Foto atau Video',
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  _onImageButtonPressed(ImageSource.camera);
+                },
+                icon: const Icon(
+                  Icons.camera,
+                  color: Color(0XFF444CE7),
+                ),
+                label: const Text(
+                  'Kamera',
+                  style: TextStyle(
+                    color: Color(0XFF444CE7),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 50),
+              TextButton.icon(
+                onPressed: () {
+                  _onImageButtonPressed(ImageSource.gallery);
+                },
+                icon: const Icon(
+                  Icons.image,
+                  color: Color(0XFF444CE7),
+                ),
+                label: const Text(
+                  'Galeri',
+                  style: TextStyle(
+                    color: Color(0XFF444CE7),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -132,43 +224,59 @@ class _DetailDonationScreenState extends State<DetailDonationScreen> {
                   ),
                 ),
                 const SizedBox(height: 16),
-                SizedBox(
-                  width: 396,
-                  height: 120,
-                  child: OutlinedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(0xFFEEF4FF),
-                      ),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                          side: BorderSide.none,
-                        ),
-                      ),
+                if (_imageFileList != null)
+                  Center(
+                    child: Image.file(
+                      File(_imageFileList![0].path),
+                      width: 372,
+                      height: 180,
+                      fit: BoxFit.cover,
                     ),
-                    onPressed: () {},
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.photo_library_outlined,
-                          color: Color(0xFF444CE7),
+                  )
+                else
+                  SizedBox(
+                    width: 396,
+                    height: 120,
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xFFEEF4FF),
                         ),
-                        const SizedBox(height: 12),
-                        Text(
-                          'Tambah foto atau video',
-                          style: GoogleFonts.inter(
-                            color: const Color(0xFF444CE7),
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            side: BorderSide.none,
                           ),
                         ),
-                      ],
+                      ),
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) => bottomSheet(),
+                        );
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.photo_library_outlined,
+                            color: Color(0xFF444CE7),
+                          ),
+                          const SizedBox(height: 12),
+                          Text(
+                            'Tambah foto atau video',
+                            style: GoogleFonts.inter(
+                              color: const Color(0xFF444CE7),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
                 const SizedBox(height: 32),
                 SizedBox(
                   width: 396,
